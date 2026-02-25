1f4ce087b2c9dae860b69e81599a7c7ca8be95ed
aryaintarann <aryaintaran09@proton.me>
2026-02-26 00:24:18 +0800

    refactor: update environment variables and adjust CDN usage in client configuration

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,11 @@
 import { createClient } from "next-sanity";
 import { apiVersion, dataset, projectId } from "../env";
 
+const useCdn = process.env.SANITY_USE_CDN !== "false";
+
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false,
+  useCdn,
 });
